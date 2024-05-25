da2b01753e78a8575763ccc7742a9491a09581e0
Darkhan Zhursin <daratechn@gmail.com>
2024-05-25 15:40:03 -0500

    lab 1

--- lab/10-spring-intro/src/test/java/rewards/internal/RewardNetworkImplTests.java
+++ lab/10-spring-intro/src/test/java/rewards/internal/RewardNetworkImplTests.java
@@ -32,7 +32,6 @@ public class RewardNetworkImplTests {
 	 */
 	private RewardNetworkImpl rewardNetwork;
 
-	// TODO-09: Review the test setup
 	@BeforeEach
 	public void setUp() throws Exception {
 		// Create stubs to facilitate fast in-memory testing with
@@ -45,11 +44,8 @@ public class RewardNetworkImplTests {
 		rewardNetwork = new RewardNetworkImpl(accountRepo, restaurantRepo, rewardRepo);
 	}
 
-	// TODO-10: Test RewardNetworkImpl class
-	// - Remove the @Disabled annotation below.
 	// - Run this JUnit test. Verify it passes.
 	@Test
-	@Disabled
 	public void testRewardForDining() {
 		// create a new dining of 100.00 charged to credit card '1234123412341234' by merchant '123457890' as test input
 		Dining dining = Dining.createDining("100.00", "1234123412341234", "1234567890");
