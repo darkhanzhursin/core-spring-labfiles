6e1451f943171e84eddff871afe79d4d8189ed2f
Darkhan Zhursin <daratechn@gmail.com>
2024-06-08 16:51:57 -0500

    Testing

--- lab/24-test/src/main/java/rewards/internal/account/JdbcAccountRepository.java
+++ lab/24-test/src/main/java/rewards/internal/account/JdbcAccountRepository.java
@@ -10,6 +10,7 @@ import javax.sql.DataSource;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Profile;
 import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.stereotype.Repository;
 
@@ -20,6 +21,7 @@ import common.money.Percentage;
  * Loads accounts from a data source using the JDBC API.
  */ 
 @Repository
+@Profile("jdbc")
 public class JdbcAccountRepository implements AccountRepository {
 
 	private final Logger logger = LoggerFactory.getLogger(getClass());
