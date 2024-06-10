ad169d1b131944a8901c5a9fb32e9214b7176d04
Darkhan Zhursin <daratechn@gmail.com>
2024-06-09 19:21:31 -0500

    configuration lab

--- lab/32-jdbc-autoconfig/src/main/java/rewards/RewardsApplication.java
+++ lab/32-jdbc-autoconfig/src/main/java/rewards/RewardsApplication.java
@@ -1,8 +1,15 @@
 package rewards;
 
+import config.RewardsConfig;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Import;
+import org.springframework.jdbc.core.JdbcTemplate;
 
 // TODO-00 : In this lab, you are going to exercise the following:
 // - Understanding how auto-configuration is triggered in Spring Boot application
@@ -35,6 +42,8 @@ import org.springframework.boot.SpringApplication;
 // TODO-13 (Optional) : Follow the instruction in the lab document.
 //           The section titled "Build and Run using Command Line tools".
 
+@SpringBootApplication(exclude = DataSourceAutoConfiguration.class)
+@Import(RewardsConfig.class)
 public class RewardsApplication {
     static final String SQL = "SELECT count(*) FROM T_ACCOUNT";
 
@@ -45,27 +54,15 @@ public class RewardsApplication {
         SpringApplication.run(RewardsApplication.class, args);
     }
 
-    // TODO-04 : Let Spring Boot execute database scripts
-    // - Move the SQL scripts (schema.sql and data.sql)
-    //   from `src/test/resources/rewards/testdb` directory
-    //   to `src/main/resources/` directory
+    @Bean
+    CommandLineRunner commandLineRunner(JdbcTemplate jdbcTemplate){
+        return args -> logger.info("Hello, there are {} accounts",
+                jdbcTemplate.queryForObject(SQL, Long.class));
+    }
 
-    // TODO-05 : Implement a command line runner that will query count from
-    //           T_ACCOUNT table and log the count to the console
-    // - Use the SQL query and logger provided above.
-    // - Use the JdbcTemplate bean that Spring Boot auto-configured for you
-    // - Run this application and verify "Hello, there are 21 accounts" log message
-    //   gets displayed in the console
-
-    // TODO-07 (Optional): Enable full debugging in order to observe how Spring Boot
-    //           performs its auto-configuration logic
-    // - Follow TO-DO-07 in application.properties, then come back here.
-    // - Run the application
-    // - In the console output, find "CONDITIONS EVALUATION REPORT".
-    //   It represents the auto-configuration logic used by Spring Boot.
-    // - Search for "JdbcTemplateAutoConfiguration matched:" and
-    //   "DataSourceAutoConfiguration matched:". Note that each @Conditional*
-    //   represents a single conditional statement in the "JdbcTemplateAutoConfiguration"
-    //   and "DataSourceAutoConfiguration" classes.
+    @Bean
+    CommandLineRunner commandLineRunner2(RewardsRecipientProperties rewardsRecipientProperties) {
+        return args -> System.out.println("Recipient: " + rewardsRecipientProperties.getName());
+    }
 
 }
--- lab/32-jdbc-autoconfig/src/test/java/rewards/SystemTestConfig.java
+++ lab/32-jdbc-autoconfig/src/test/java/rewards/SystemTestConfig.java
@@ -16,23 +16,10 @@ import javax.sql.DataSource;
 @Configuration
 @Import(RewardsConfig.class)
 public class SystemTestConfig {
-	private final Logger logger = LoggerFactory.getLogger(SystemTestConfig.class);
-
 	// TODO-08 : Use Spring Boot auto-configuration for DataSource
 	// - Note that this test configuration class is used by
 	//   RewardNetworkTests in the same package
 	// - Comment out the @Bean method below so this method
 	//   is no longer called
-
-	@Bean
-	public DataSource dataSource() {
-		logger.debug("Creating the datasource bean explicitly");
-
-		return
-			(new EmbeddedDatabaseBuilder())
-			.addScript("classpath:rewards/testdb/schema.sql")
-			.addScript("classpath:rewards/testdb/data.sql")
-			.build();
-	}
 	
 }
