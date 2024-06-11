3bdfa06980da2f73f298aa8dc0d54ffa955924d4
Darkhan Zhursin <daratechn@gmail.com>
2024-06-11 16:38:18 -0500

    mvc lab

--- lab/36-mvc/src/test/java/accounts/web/AccountControllerTests.java
+++ lab/36-mvc/src/test/java/accounts/web/AccountControllerTests.java
@@ -28,7 +28,7 @@ public class AccountControllerTests {
 
 	// TODO-07: Remove the @Disabled annotation, run the test, it should now pass.
 	@Test
-	@Disabled
+	//@Disabled
 	public void testHandleListRequest() {
 		List<Account> accounts = controller.accountList();
 
@@ -44,16 +44,19 @@ public class AccountControllerTests {
 
 	// TODO-10a: Remove the @Disabled annotation, run the test, it should pass.
 	@Test
-	@Disabled
+	//@Disabled
 	public void testHandleDetailsRequest() {
 		// TODO-09a: Implement test code which calls the accountDetails() method on the controller.
 		// - It will take one parameter - use "expectedAccountId" defined above
 		// - It will return an Account
-
+		Account account = controller.accountDetails(expectedAccountId);
 		// TODO-09b: Define the following assertions:
 		// - The account is not null
 		// - The account id matches "expectedAccountId" defined above
 		// - The account number matches "expectedAccountNumber" defined above
+		assertNotNull(account);
+		assertEquals(expectedAccountId, (long) account.getEntityId());
+		assertEquals(expectedAccountNumber, account.getNumber());
 	}
 
 }
